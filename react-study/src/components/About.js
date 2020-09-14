a3be3af06c97139314f28a2303836031d02ef63a
wangs <1611462844@qq.com>
2020-09-14 18:42:48 +0800

    'add'

--- react-study/src/components/About.js
+++ react-study/src/components/About.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+
+const friendList = [
+    {id:1,name:'wang'},
+    {id:2,name:'kai'},
+    {id:3,name:'sen'},
+];
+const onlineStatus = {
+    1:1,
+    2:0,
+    3:0
+}
+
+function useFriendStatus(id){
+    const [isOnline,setIsOnline] = useState(null);
+    console.log(isOnline)
+    useEffect(() => {
+        setIsOnline(onlineStatus[id])
+    },[id])
+
+    return isOnline;
+}
+
+
 
 function About(){
+    const [selectID,setSelectID] = useState(1);
+    const isOnline = useFriendStatus(selectID);
+    console.log("isOnline:"+isOnline)
+    const styles = {
+        status:{
+            width:'10px',
+            height:'10px',
+            borderRadius:'50%',
+            display:'inline-block',
+            backgroundColor:isOnline?'green':'red'
+        }
+    }
+
     return (
-        <div>About</div>
+        <div>
+            <span>状态：</span>
+            <span style={styles.status}></span>
+            <br/>
+            <select value={selectID} onChange={e => {setSelectID(e.target.value)}}>
+                {friendList.map(item => {
+                    return (
+                        <option key={item.id} value={item.id}>
+                            {item.name}
+                        </option>
+                    )
+                })}
+            </select>
+        </div>
     )
 }
 
